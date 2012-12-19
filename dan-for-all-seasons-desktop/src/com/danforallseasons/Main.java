55dd7def3e89d7146c6852249342e8a4183f9c9c
Tyler Sutherland <tylerksutherland@gmail.com>
2012-12-19 18:00:30 -0500

    Initial Commit

---
+++ dan-for-all-seasons-desktop/src/com/danforallseasons/Main.java
@@ -0,0 +1,16 @@
+package com.danforallseasons;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "dan-for-all-seasons";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new DanForAllSeasons(), cfg);
+	}
+}
