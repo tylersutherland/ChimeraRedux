c6f0429afeaf6d94bdffdeeb99b48303f430f743
Tyler Sutherland <tylerksutherland@gmail.com>
2013-03-08 16:06:52 -0500

    The shader does a half fade, don't know how to make it fade over time yet

--- dan-for-all-seasons-desktop/bin/shaders/fadeOutShader.fs
+++
@@ -1,8 +0,0 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-void main()
-{
-	gl_Color = vec4(1.0, 1.0, 0.0, 1.0);
-}
--- dan-for-all-seasons/src/com/danforallseasons/tiled/TiledMapRenderer.java
+++ dan-for-all-seasons/src/com/danforallseasons/tiled/TiledMapRenderer.java
@@ -1,12 +1,14 @@
 package com.danforallseasons.tiled;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Timer;
 
 public class TiledMapRenderer implements Disposable {
 
@@ -34,6 +36,25 @@ public class TiledMapRenderer implements Disposable {
 
 	private int oldLayer;
 
+	private float elapsedTime;
+
+	String vertexShader = "attribute vec4 a_position;    \n"
+			+ "attribute vec4 a_color;\n" + "attribute vec2 a_texCoord0;\n"
+			+ "uniform mat4 u_worldView;\n" + "varying vec4 v_color;"
+			+ "varying vec2 v_texCoords;" + "void main()                  \n"
+			+ "{                            \n"
+			+ "   v_color = vec4(1, 1, 1, 1); \n"
+			+ "   v_texCoords = a_texCoord0; \n"
+			+ "   gl_Position =  u_worldView * a_position;  \n"
+			+ "}                            \n";
+	String fragmentShader = "#ifdef GL_ES\n" + "precision mediump float;\n"
+			+ "#endif\n" + "varying vec4 v_color;\n"
+			+ "varying vec2 v_texCoords;\n" + "uniform sampler2D u_texture;\n"
+			+ "void main()                                  \n"
+			+ "{                                            \n"
+			+ "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n"
+			+ "}";
+
 	private ShaderProgram fadeOutShader;
 	private ShaderProgram defaultShader;
 
@@ -48,8 +69,8 @@ public class TiledMapRenderer implements Disposable {
 
 		defaultShader = SpriteBatch.createDefaultShader();
 		fadeOutShader = new ShaderProgram(
-				Gdx.files.internal("shaders/fadeOutShader.vs"),
-				Gdx.files.internal("shaders/fadeOutShader.fs"));
+				Gdx.files.internal("shaders/fadeOutShader.vert"),
+				Gdx.files.internal("shaders/fadeOutShader.frag"));
 
 	}
 
@@ -102,7 +123,9 @@ public class TiledMapRenderer implements Disposable {
 						batch.draw(atlas.getRegion(oldTileId), j, i, 0, 0,
 								tileWidth, tileHeight, 1f / PIXELS_PER_METER,
 								1f / PIXELS_PER_METER, 0);
-
+						elapsedTime += Gdx.graphics.getDeltaTime();
+						if (elapsedTime > 30)
+							changeLayer = false;
 					}
 				}
 
@@ -133,6 +156,7 @@ public class TiledMapRenderer implements Disposable {
 
 	public void changeLayer() {
 		changeLayer = true;
+		elapsedTime = 0;
 		oldLayer = currentLayer;
 		currentLayer++;
 		currentLayer %= map.layers.size();
