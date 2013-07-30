fe472f24ba8b01bc34e608b19d608fa8e87e9de7
Tyler Sutherland <tylerksutherland@gmail.com>
2013-07-29 21:38:12 -0400

    new map rendering added

--- dan-for-all-seasons-desktop/src/com/danforallseasons/Main.java
+++ dan-for-all-seasons-desktop/src/com/danforallseasons/Main.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "dan-for-all-seasons-newlibgdx";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.title = "dan-for-all-seasons";
+		cfg.useGL20 = true;
+		cfg.width = 800;
+		cfg.height = 600;
 		
 		new LwjglApplication(new DanForAllSeasons(), cfg);
 	}
--- dan-for-all-seasons/src/com/danforallseasons/screens/DemoScreen.java
+++ dan-for-all-seasons/src/com/danforallseasons/screens/DemoScreen.java
@@ -2,107 +2,55 @@ package com.danforallseasons.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.EdgeShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 import com.danforallseasons.DanForAllSeasons;
-import com.danforallseasons.PhysicsDan;
-import com.danforallseasons.physics.PhysicsHelper;
-import com.danforallseasons.tiled.TiledMapAtlas;
-import com.danforallseasons.tiled.TiledMapRenderer;
 
 public class DemoScreen implements Screen {
-	public static final int TILE_WIDTH = 64;
-	public static final int TILE_HEIGHT = 64;
-	private static final int PIXELS_PER_METER = TiledMapRenderer.PIXELS_PER_METER;
+
 	private static final String PAUSE_MSG = "Game Paused\r\nPress R to Resume";
 
-	/* Map */
-	private TiledMap map;
-	private TiledMapAtlas atlas;
-	private TiledMapRenderer mapRenderer;
-
+	private int width = Gdx.graphics.getWidth();
+	private int height = Gdx.graphics.getHeight();
+	private float unitScale = 1 / 32f;
 	/* Rendering */
 	private SpriteBatch spriteBatch;
+	private TiledMap tMap;
+	private OrthogonalTiledMapRenderer tMapRenderer;
 
 	private SpriteBatch fontSpriteBatch;
 	private BitmapFont font;
 	private OrthographicCamera cam;
 
-	/* Physics */
-	private World world;
-	private Array<Body> groundBodies;
-	private Box2DDebugRenderer physicsDebugRenderer;
-
-	private PhysicsDan pd;
-
 	private boolean gamePaused;
-	
-	private DanForAllSeasons dan;
+
+	private DanForAllSeasons game;
 
 	public DemoScreen(DanForAllSeasons dan) {
-		this.dan = dan;
+		game = dan;
 		spriteBatch = new SpriteBatch();
 		fontSpriteBatch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.MAGENTA);
-		map = TiledLoader.createMap(Gdx.files.internal("map/demo.tmx"));
-		atlas = new TiledMapAtlas(map, Gdx.files.internal("map/demo.pack"));
-		setupPhysics();
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, width * unitScale, height * unitScale);
+		cam.position.set(12.5f, 45, 1);
+		cam.update();
 
-		mapRenderer = new TiledMapRenderer(map, atlas);
-		physicsDebugRenderer = new Box2DDebugRenderer();
-		cam = new OrthographicCamera(
-				Gdx.graphics.getWidth() / PIXELS_PER_METER,
-				Gdx.graphics.getHeight() / PIXELS_PER_METER);
-
-		cam.setToOrtho(true, cam.viewportWidth, cam.viewportHeight);
-		cam.position.set(5, 6, 0);
-
+		tMap = new TmxMapLoader().load("map/demo.tmx");
+		tMapRenderer = new OrthogonalTiledMapRenderer(tMap, unitScale);
 		gamePaused = false;
-	}
-
-	private void setupPhysics() {
-		world = new World(new Vector2(0, 10), true);
-		groundBodies = new Array<Body>();
-		Array<Vector2[]> groundVertices = PhysicsHelper.getCollisionShapes(map);
-		for (int i = 0; i < groundVertices.size; i++) {
-
-			EdgeShape groundPoly = new EdgeShape();
-			Vector2 a = groundVertices.get(i)[0];
-			Vector2 b = groundVertices.get(i)[1];
-			groundPoly.set(a, b);
-			BodyDef groundBodyDef = new BodyDef();
-			groundBodyDef.type = BodyType.StaticBody;
-			Body groundBody = world.createBody(groundBodyDef);
-
-			FixtureDef fixtureDef = new FixtureDef();
-			fixtureDef.shape = groundPoly;
-			fixtureDef.filter.groupIndex = 0;
-			fixtureDef.friction = 1f;
-			groundBody.createFixture(fixtureDef);
-			groundPoly.dispose();
-
-			groundBodies.add(groundBody);
-		}
-		pd = new PhysicsDan(10, 10, world);
 	}
 
 	@Override
@@ -111,92 +59,94 @@ public class DemoScreen implements Screen {
 		Gdx.gl.glClearColor(0, 1, 1, 1);
 
 		if (gamePaused) {
-			Gdx.gl.glClearColor(0.5f, 0.9f, 0.9f, 1);
-
-			fontSpriteBatch.begin();
-			font.drawMultiLine(fontSpriteBatch, PAUSE_MSG,
-					Gdx.graphics.getWidth() / 2
-							- font.getBounds(PAUSE_MSG).width / 2 + 50,
-					Gdx.graphics.getHeight() / 2
-							+ font.getBounds(PAUSE_MSG).height / 2);
-			fontSpriteBatch.end();
-
-			if (Gdx.input.isKeyPressed(Keys.R)) {
-				Gdx.app.log(DanForAllSeasons.LOG, "Resuming Game");
-				resumeGame();
-			}
+			renderPauseMenu();
+		} else {
+			renderGame(delta);
 		}
+	}
 
-		if (!gamePaused) {
+	private void renderGame(float delta) {
 
-			if (Gdx.input.isKeyPressed(Keys.P)) {
-				Gdx.app.log(DanForAllSeasons.LOG, "Pausing Game");
-				pauseGame();
-			}
+		renderMap();
 
-			if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
-				Gdx.app.log(DanForAllSeasons.LOG, "Quitting Game");
-				Gdx.app.exit();
-			}
+		renderDebugHUD();
 
-			if (Gdx.input.isKeyPressed(Keys.R)) {
-				Gdx.app.log(DanForAllSeasons.LOG, "Restting Demo");
-				dan.setScreen(new DemoScreen(dan));
-			}
-			
-			mapRenderer.render(cam);
-			physicsDebugRenderer.render(world, cam.combined);
+		update(delta);
+	}
 
-			spriteBatch.setProjectionMatrix(cam.combined);
-			spriteBatch.begin();
-			{
-				pd.draw(spriteBatch);
-			}
-			spriteBatch.end();
+	private void renderMap() {
+		tMapRenderer.setView(cam);
+		tMapRenderer.render();
+	}
 
-			fontSpriteBatch.begin();
-			{
-				font.draw(fontSpriteBatch,
-						"FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
-				font.draw(fontSpriteBatch,
-						"Initial Col, Last Col " + mapRenderer.getInitialCol()
-								+ "," + mapRenderer.getLastCol(), 20, 60);
-				font.draw(fontSpriteBatch,
-						"Initial Row, Last Row " + mapRenderer.getInitialRow()
-								+ "," + mapRenderer.getLastRow(), 20, 40);
-				font.draw(fontSpriteBatch, "Location: " + cam.position.x + ","
-						+ cam.position.y, 20, 80);
-				font.draw(
-						fontSpriteBatch,
-						"Press P to Pause",
-						Gdx.graphics.getWidth()
-								- font.getBounds("Press P to Pause").width,
-						Gdx.graphics.getHeight());				
-				font.draw(
-						fontSpriteBatch, 
-						"Press Esc to Quit", 0,
-						Gdx.graphics.getHeight());
-				
-				font.draw(
-						fontSpriteBatch, 
-						"Press R to Restart", Gdx.graphics.getWidth() / 2
-								- font.getBounds("Press R to Restart").width / 2,
-						Gdx.graphics.getHeight());
-						
-			}
-			fontSpriteBatch.end();
+	private void renderDebugHUD() {
+		fontSpriteBatch.begin();
+		{
+			font.draw(fontSpriteBatch,
+					"FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
+			font.draw(fontSpriteBatch, "Location: " + cam.position.x + ","
+					+ cam.position.y, 20, 80);
+			font.draw(fontSpriteBatch, "Zoom: " + cam.zoom, 20, 60);
+			font.draw(
+					fontSpriteBatch,
+					"Press P to Pause",
+					Gdx.graphics.getWidth()
+							- font.getBounds("Press P to Pause").width,
+					Gdx.graphics.getHeight());
+			font.draw(fontSpriteBatch, "Press Esc to Quit", 0,
+					Gdx.graphics.getHeight());
 
-			update(delta);
+			font.draw(
+					fontSpriteBatch,
+					"Press R to Restart",
+					Gdx.graphics.getWidth() / 2
+							- font.getBounds("Press R to Restart").width / 2,
+					Gdx.graphics.getHeight());
+
+		}
+		fontSpriteBatch.end();
+	}
+
+	private void renderPauseMenu() {
+		Gdx.gl.glClearColor(0.5f, 0.9f, 0.9f, 1);
+
+		fontSpriteBatch.begin();
+		font.drawMultiLine(fontSpriteBatch, PAUSE_MSG, Gdx.graphics.getWidth()
+				/ 2 - font.getBounds(PAUSE_MSG).width / 2 + 50,
+				Gdx.graphics.getHeight() / 2 + font.getBounds(PAUSE_MSG).height
+						/ 2);
+		fontSpriteBatch.end();
+
+		if (Gdx.input.isKeyPressed(Keys.R)) {
+			Gdx.app.log(DanForAllSeasons.LOG, "Resuming Game");
+			resumeGame();
 		}
 	}
 
 	private void update(float delta) {
-		world.step(delta, 4, 4);
 		Input input = Gdx.input;
-		pd.update(input, delta);
-		cam.position.set(pd.getPosition());
+
+		if (input.isKeyPressed(Keys.P)) {
+			Gdx.app.log(DanForAllSeasons.LOG, "Pausing Game");
+			pauseGame();
+		}
+		if (input.isKeyPressed(Keys.ESCAPE)) {
+			Gdx.app.log(DanForAllSeasons.LOG, "Quitting Game");
+			Gdx.app.exit();
+		}
+
+		if (input.isKeyPressed(Keys.R)) {
+			Gdx.app.log(DanForAllSeasons.LOG, "Restting Demo");
+			game.setScreen(new DemoScreen(game));
+		}
+
+		if (input.isKeyPressed(Keys.UP)) cam.translate(new Vector2(0, 1));
+		if (input.isKeyPressed(Keys.DOWN)) cam.translate(new Vector2(0, -1));
+		if (input.isKeyPressed(Keys.LEFT)) cam.translate(new Vector2(1, 0));
+		if (input.isKeyPressed(Keys.RIGHT)) cam.translate(new Vector2(-1, 0));
+		if (input.isKeyPressed(Keys.Q)) cam.zoom += 0.1f;
+		if (input.isKeyPressed(Keys.E)) cam.zoom -= 0.1f;
 		cam.update();
-		if (input.justTouched()) mapRenderer.changeLayer();
 	}
 
 	private void pauseGame() {
@@ -209,19 +159,16 @@ public class DemoScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -232,14 +179,11 @@ public class DemoScreen implements Screen {
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 
 	}
-
 }
