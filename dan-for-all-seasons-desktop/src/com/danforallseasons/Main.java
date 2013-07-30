65098606ed2ddf57e0cc884807f69fb728c41f90
Tyler Sutherland <tylerksutherland@gmail.com>
2013-07-29 20:34:13 -0400

    initial commit after update

--- dan-for-all-seasons-desktop/src/com/danforallseasons/Main.java
+++ dan-for-all-seasons-desktop/src/com/danforallseasons/Main.java
@@ -6,10 +6,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "dan-for-all-seasons";
-		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 600;
+		cfg.title = "dan-for-all-seasons-newlibgdx";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
 		new LwjglApplication(new DanForAllSeasons(), cfg);
 	}
 }
